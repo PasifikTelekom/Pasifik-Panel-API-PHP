9b9d319eabe468b845c8f1c4a39d4e34d466f337
Tarek Kalaji <tarek-aec@hotmail.com>
2016-09-01 18:33:03 +0300

    Update test.php

--- main/test.php
+++ main/test.php
@@ -30,7 +30,7 @@ class TestCase
 		$to = "905999999998,905999999999";
 		$text = "SMS Test";
 		$scheduled_delivery_time = "2016-09-28T09:30:00Z";// "%Y-%m-%dT%H:%M:%SZ" format e.g "2016-07-23T21:54:02Z" in UTC Timezone.
-		$period = 1440; // minute number e.g 1440 minute for 24 hours
+		$period = 1440; // minutes number e.g 1440 minutes for 24 hours
 		$result = $this->obj->submit($from, $to, $text, false, "Default", $scheduled_delivery_time, $period);
 	}
 	public function send_one_message_to_many_receipients_turkish_language(){
